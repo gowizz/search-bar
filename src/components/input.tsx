b959b02892dd5e73ea7103e7bb458f0b83377441
karl96k <karl.gustav1789@gmail.com>
2020-07-21 23:47:17 +0300

    Convert input to class

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,6 +1,6 @@
 import '../assets/input.css';
 
-import React, { FunctionComponent } from 'react';
+import React, { Component, FunctionComponent } from 'react';
 import { CancelIcon, SearchIcon } from '../assets/icons';
 
 interface InputProps {
@@ -13,35 +13,50 @@ interface InputProps {
 
 // TODO: use props autofocus
 
-export const Input: FunctionComponent<InputProps> = ({
-  query,
-  placeholder,
-  showInputSearchIcon,
-  onChange,
-  onCancel,
-}) => {
-  return (
-    <>
-      <div className="search_icon">{showInputSearchIcon && <SearchIcon />}</div>
-      <div className="search_input">
-        <input
-          spellCheck={false}
-          value={query}
-          placeholder={placeholder}
-          onChange={onChange}
-          autoComplete="off"
-          name="query"
-          type="text"
-          title="Search with Gowiz"
-          aria-required="true"
-        />
-      </div>
-      <div className="cancel_icon" title={'Clear'} onClick={onCancel}>
-        {query != undefined && query.length > 0 && <CancelIcon />}
-      </div>
-    </>
-  );
-};
+export class Input extends Component<InputProps> {
+  componentDidUpdate(prevProps: Readonly<InputProps>, prevState: Readonly<{}>, snapshot?: any): void {
+    console.count('Input');
+  }
+  shouldComponentUpdate(nextProps: Readonly<InputProps>, nextState: Readonly<{}>, nextContext: any): boolean {
+    if (this.props.query != nextProps.query) {
+      return true;
+    }
+    console.log('Current props');
+    console.log(this.props);
+    console.log('next props');
+    console.log(nextProps);
+
+    return false;
+  }
+
+  render() {
+    let { query, placeholder, showInputSearchIcon, onChange, onCancel } = this.props;
+    //TODO: add prios for title,aria_label
+
+    return (
+      <>
+        <div className="search_icon">{showInputSearchIcon && <SearchIcon />}</div>
+        <div className="search_input">
+          <input
+            spellCheck={false}
+            value={query}
+            placeholder={placeholder}
+            onChange={onChange}
+            autoComplete="off"
+            name="query"
+            type="text"
+            title="Search with Gowiz"
+            aria-required="true"
+            aria-label="Search query input"
+          />
+        </div>
+        <div className="cancel_icon" title={'Clear'} onClick={onCancel}>
+          {query != undefined && query.length > 0 && <CancelIcon />}
+        </div>
+      </>
+    );
+  }
+}
 
 //TODO: add testing
 //TODO: previously search values should be deleted from localstorage
