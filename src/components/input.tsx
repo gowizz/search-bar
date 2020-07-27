11faa03164e6bc176695e69fc057c7410c66914d
karl96k <karl.gustav1789@gmail.com>
2020-07-27 21:21:28 +0300

    test and update input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent } from 'react';
 import { CancelIcon, SearchIcon } from '../assets/icons';
-import shallowCompare from 'react-addons-shallow-compare';
 
 interface InputProps {
   query?: string;
@@ -9,59 +8,68 @@ interface InputProps {
   onCancel: (e: any) => void;
   showInputSearchIcon?: boolean;
   useAutoFocus?: boolean;
+  useDarkTheme?: boolean;
 }
 
-export const SearchIconClass: FunctionComponent<{ showInputSearchIcon: boolean }> = ({ showInputSearchIcon }) => {
+export const SearchIconClass: FunctionComponent<{ useDarkTheme?: boolean; showInputSearchIcon: boolean }> = ({
+  useDarkTheme = false,
+  showInputSearchIcon,
+}) => {
   if (showInputSearchIcon) {
+    const classname = useDarkTheme ? 'search_icon dark_search_icon' : 'search_icon';
     return (
-      <div className="search_icon">
-        <SearchIcon />
+      <div className={classname}>
+        <SearchIcon data-testid={'search_icon_svg'} />
       </div>
     );
   }
   return null;
 };
 
-export const CancelIconClass: FunctionComponent<{ query: string; onCancel: (e: any) => void }> = ({
-  query,
-  onCancel,
-}) => {
+export const CancelIconClass: FunctionComponent<{
+  useDarkTheme?: boolean;
+  query: string;
+  onCancel: (e: any) => void;
+}> = ({ useDarkTheme = false, query, onCancel }) => {
   const cancel_button_is_needed = query != undefined && query.length > 0;
   if (cancel_button_is_needed) {
+    const classname = useDarkTheme ? 'cancel_icon dark_cancel_icon' : 'cancel_icon';
+
     return (
-      <div id={'cancel_icon'} tabIndex={2} className="cancel_icon" title={'Clear'} onClick={onCancel}>
-        <CancelIcon />
+      <div id={'cancel_icon'} tabIndex={2} className={classname} title={'Clear'} onClick={onCancel}>
+        <CancelIcon data-testid={'cancel_icon_svg'} />
       </div>
     );
   }
   return null;
 };
 
-export default class Input extends React.Component<InputProps> {
-  shouldComponentUpdate(nextProps: Readonly<InputProps>, nextState: Readonly<{}>, nextContext: any): boolean {
-    if (this.props.query != nextProps.query) {
-      return true;
-    }
-    return shallowCompare(this, nextProps, nextState);
-  }
-
+export default class Input extends React.PureComponent<InputProps> {
   render() {
-    const {
-      query = '',
-      placeholder = 'Search on Gowiz',
-      showInputSearchIcon = true,
-      onChange,
-      onCancel,
-      useAutoFocus = true,
-    } = this.props;
+    const { showInputSearchIcon = true, onChange, onCancel, useAutoFocus = false, useDarkTheme = false } = this.props;
 
-    const title = query.length != null && query.length > 0 ? 'Search ' + query + ' on Gowiz' : 'Search on Gowiz';
+    let placeholder: string;
+    switch (this.props.placeholder) {
+      case undefined:
+        placeholder = 'Search on Gowiz';
+        break;
+      case null:
+        placeholder = '';
+        break;
+      default:
+        placeholder = this.props.placeholder;
+    }
+    const query = this.props.query === undefined || this.props.query === null ? '' : this.props.query.trim();
+    const title = query.length === 0 ? 'Search on Gowiz' : 'Search ' + query + ' on Gowiz';
+
+    const container_class = useDarkTheme ? 'search_input dark_input' : 'search_input';
 
     return (
       <>
-        <SearchIconClass showInputSearchIcon={showInputSearchIcon} />
-        <div className="search_input">
+        <SearchIconClass useDarkTheme={useDarkTheme} showInputSearchIcon={showInputSearchIcon} />
+        <div className={container_class}>
           <input
+            data-testid={'search_input_input'}
             spellCheck={true}
             value={query}
             placeholder={placeholder}
@@ -76,10 +84,11 @@ export default class Input extends React.Component<InputProps> {
             tabIndex={1}
           />
         </div>
-        <CancelIconClass query={query} onCancel={(e) => onCancel(e)} />
+        <CancelIconClass query={this.props.query} onCancel={(e) => onCancel(e)} useDarkTheme={useDarkTheme} />
       </>
     );
   }
 }
 
-//TODO: add testing
+//TODO: add testing on click and cancel and focus
+//TODO: add filterin
---
+++ test/components/input.test.tsx
@@ -0,0 +1,289 @@
+import React from 'react';
+
+import '@testing-library/jest-dom/extend-expect';
+import Input from '../../src/components/input';
+import { cleanup, fireEvent, render } from '@testing-library/react';
+
+describe('Input', () => {
+  const onCancel = jest.fn();
+  const onChange = jest.fn();
+  afterEach(cleanup);
+
+  describe('Default props', () => {
+    it('Input has search icon', async () => {
+      let { container } = render(<Input onChange={onChange} onCancel={onCancel} />);
+      expect(container.firstChild).toHaveClass('search_icon');
+      expect(container.firstChild.hasChildNodes()).toBe(true);
+      expect(container.childNodes.length).toBe(2);
+
+      const svg = container.firstChild.childNodes[0];
+      expect(svg.nodeName).toBe('svg');
+    });
+
+    it('Input does not have cancel button', async () => {
+      let { container } = render(<Input onChange={onChange} onCancel={onCancel} />);
+      expect(container.childNodes.length).toBe(2);
+    });
+
+    it('Default props are set correctly', async () => {
+      let { container, queryByTestId } = render(<Input onChange={onChange} onCancel={onCancel} />);
+      expect(container.childNodes.length).toBe(2);
+
+      const input_class = container.childNodes[1];
+
+      expect(input_class).toHaveClass('search_input');
+      expect(input_class.childNodes.length).toBe(1);
+
+      const input = queryByTestId('search_input_input');
+
+      const spellcheck = input.getAttribute('spellcheck');
+      const placeholder = input.getAttribute('placeholder');
+      const autocomplete = input.getAttribute('autocomplete');
+      const name = input.getAttribute('name');
+      const type = input.getAttribute('type');
+      const title = input.getAttribute('title');
+      const aria_required = input.getAttribute('aria-required');
+      const aria_label = input.getAttribute('aria-label');
+      const tabindex = input.getAttribute('tabindex');
+      const value = input.getAttribute('value');
+
+      expect(input.getAttributeNames().length).toBe(11);
+      expect(spellcheck).toBe('true');
+      expect(placeholder).toBe('Search on Gowiz');
+      expect(autocomplete).toBe('off');
+      expect(name).toBe('query');
+      expect(type).toBe('text');
+      expect(title).toBe('Search on Gowiz');
+      expect(aria_required).toBe('true');
+      expect(aria_label).toBe('Search query input');
+      expect(tabindex).toBe('1');
+      expect(value).toBe('');
+    });
+  });
+
+  describe('Custom props', () => {
+    it('Input renders without search icon', async () => {
+      let { container } = render(<Input showInputSearchIcon={false} onChange={onChange} onCancel={onCancel} />);
+
+      expect(container.childNodes.length).toBe(1);
+      expect(container.firstChild).toHaveClass('search_input');
+      expect(container.firstChild.hasChildNodes()).toBe(true);
+
+      const input = container.firstChild.childNodes[0];
+      expect(input.nodeName).toBe('INPUT');
+    });
+  });
+
+  describe('Query', () => {
+    it('Input query can be set', async () => {
+      let { container, queryByTestId } = render(<Input query={'test'} onChange={onChange} onCancel={onCancel} />);
+      expect(container.childNodes.length).toBe(3);
+
+      const input_class = container.childNodes[1];
+
+      expect(input_class).toHaveClass('search_input');
+      expect(input_class.childNodes.length).toBe(1);
+
+      const input = queryByTestId('search_input_input');
+
+      const value = input.getAttribute('value');
+      expect(value).toBe('test');
+    });
+
+    it('Input query changes title', async () => {
+      let { queryByTestId } = render(<Input query={'test'} onChange={onChange} onCancel={onCancel} />);
+      const input = queryByTestId('search_input_input');
+      const title = input.getAttribute('title');
+
+      expect(title).toBe('Search test on Gowiz');
+    });
+
+    it('Undefined query is handled correctly', async () => {
+      let { container, queryByTestId } = render(<Input query={undefined} onChange={onChange} onCancel={onCancel} />);
+      expect(container.childNodes.length).toBe(2);
+
+      const input_class = container.childNodes[1];
+
+      expect(input_class).toHaveClass('search_input');
+      expect(input_class.childNodes.length).toBe(1);
+
+      const input = queryByTestId('search_input_input');
+
+      const value = input.getAttribute('value');
+      const title = input.getAttribute('title');
+
+      expect(value).toBe('');
+      expect(title).toBe('Search on Gowiz');
+    });
+
+    it('Null query is handled correctly', async () => {
+      let { container, queryByTestId } = render(<Input query={null} onChange={onChange} onCancel={onCancel} />);
+      expect(container.childNodes.length).toBe(2);
+
+      const input_class = container.childNodes[1];
+
+      expect(input_class).toHaveClass('search_input');
+      expect(input_class.childNodes.length).toBe(1);
+
+      const input = queryByTestId('search_input_input');
+
+      const value = input.getAttribute('value');
+      const title = input.getAttribute('title');
+
+      expect(value).toBe('');
+      expect(title).toBe('Search on Gowiz');
+    });
+
+    it('Empty query is handled correctly', async () => {
+      let { container, queryByTestId } = render(<Input query={''} onChange={onChange} onCancel={onCancel} />);
+      expect(container.childNodes.length).toBe(2);
+
+      const input_class = container.childNodes[1];
+
+      expect(input_class).toHaveClass('search_input');
+      expect(input_class.childNodes.length).toBe(1);
+
+      const input = queryByTestId('search_input_input');
+
+      const value = input.getAttribute('value');
+      const title = input.getAttribute('title');
+
+      expect(value).toBe('');
+      expect(title).toBe('Search on Gowiz');
+    });
+
+    it('First space bar in query is treated correctly', async () => {
+      let { container, queryByTestId } = render(<Input query={'          '} onChange={onChange} onCancel={onCancel} />);
+      expect(container.childNodes.length).toBe(3);
+
+      const input_class = container.childNodes[1];
+
+      expect(input_class).toHaveClass('search_input');
+      expect(input_class.childNodes.length).toBe(1);
+
+      const input = queryByTestId('search_input_input');
+
+      const value = input.getAttribute('value');
+      const title = input.getAttribute('title');
+
+      expect(value).toBe('');
+      expect(title).toBe('Search on Gowiz');
+    });
+  });
+
+  describe('Placeholder', () => {
+    it('Placeholder query can be set', async () => {
+      let { container, queryByTestId } = render(
+        <Input placeholder={'Search...'} onChange={onChange} onCancel={onCancel} />
+      );
+      expect(container.childNodes.length).toBe(2);
+
+      const input_class = container.childNodes[1];
+
+      expect(input_class).toHaveClass('search_input');
+      expect(input_class.childNodes.length).toBe(1);
+
+      const input = queryByTestId('search_input_input');
+
+      const value = input.getAttribute('placeholder');
+      expect(value).toBe('Search...');
+    });
+
+    it('Placeholder does not changes title', async () => {
+      let { queryByTestId } = render(<Input placeholder={'Search...'} onChange={onChange} onCancel={onCancel} />);
+      const input = queryByTestId('search_input_input');
+      const title = input.getAttribute('title');
+
+      expect(title).toBe('Search on Gowiz');
+    });
+
+    it('Undefined placeholder is handled correctly', async () => {
+      let { container, queryByTestId } = render(
+        <Input query={'test'} placeholder={undefined} onChange={onChange} onCancel={onCancel} />
+      );
+      expect(container.childNodes.length).toBe(3);
+
+      const input_class = container.childNodes[1];
+
+      expect(input_class).toHaveClass('search_input');
+      expect(input_class.childNodes.length).toBe(1);
+
+      const input = queryByTestId('search_input_input');
+
+      const placeholder = input.getAttribute('placeholder');
+
+      expect(placeholder).toBe('Search on Gowiz');
+    });
+
+    it('Null placeholder is handled correctly', async () => {
+      let { container, queryByTestId } = render(
+        <Input query={'test'} placeholder={null} onChange={onChange} onCancel={onCancel} />
+      );
+      expect(container.childNodes.length).toBe(3);
+
+      const input_class = container.childNodes[1];
+
+      expect(input_class).toHaveClass('search_input');
+      expect(input_class.childNodes.length).toBe(1);
+
+      const input = queryByTestId('search_input_input');
+
+      const value = input.getAttribute('value');
+      const title = input.getAttribute('title');
+      const placeholder = input.getAttribute('placeholder');
+
+      expect(value).toBe('test');
+      expect(title).toBe('Search test on Gowiz');
+      expect(placeholder).toBe('');
+    });
+
+    it('Empty placeholder is handled correctly', async () => {
+      let { container, queryByTestId } = render(
+        <Input query={''} placeholder={undefined} onChange={onChange} onCancel={onCancel} />
+      );
+      expect(container.childNodes.length).toBe(2);
+
+      const input_class = container.childNodes[1];
+
+      expect(input_class).toHaveClass('search_input');
+      expect(input_class.childNodes.length).toBe(1);
+
+      const input = queryByTestId('search_input_input');
+
+      const placeholder = input.getAttribute('placeholder');
+
+      expect(placeholder).toBe('Search on Gowiz');
+    });
+
+    it('Placeholder does not change space bar', async () => {
+      let { container, queryByTestId } = render(
+        <Input placeholder={' Search...  '} onChange={onChange} onCancel={onCancel} />
+      );
+      expect(container.childNodes.length).toBe(2);
+
+      const input_class = container.childNodes[1];
+
+      expect(input_class).toHaveClass('search_input');
+      expect(input_class.childNodes.length).toBe(1);
+
+      const input = queryByTestId('search_input_input');
+
+      const placeholder = input.getAttribute('placeholder');
+
+      expect(placeholder).toBe(' Search...  ');
+    });
+  });
+
+  describe('Cancel', () => {
+    it('Cancel function can be called', async () => {
+      let { container } = render(<Input query={'test'} onChange={onChange} onCancel={onCancel} />);
+      expect(container.childNodes.length).toBe(3);
+
+      const cancel = container.childNodes[2];
+
+      fireEvent.click(cancel);
+      expect(onCancel).toBeCalled();
+      expect(onCancel).toBeCalledTimes(1);
+    });
+  });
+});
